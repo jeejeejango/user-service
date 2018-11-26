8dd3d5a6fccf349aa9e370447e82e050a70d7561
jeejeejango <boonping.teo@oracle.com>
2018-11-26 17:48:17 +0630

    update wercker.yml

--- src/test/java/org/jeejeejango/UserServiceApplicationTests.java
+++ src/test/java/org/jeejeejango/UserServiceApplicationTests.java
@@ -1,14 +1,13 @@
 package org.jeejeejango;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@ActiveProfiles("dev")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
 public class UserServiceApplicationTests {
 
     //@Test
