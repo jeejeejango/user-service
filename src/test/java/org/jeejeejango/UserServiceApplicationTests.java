a8658eb014da955f173251c740e101f2866fcf85
jeejeejango <boonping.teo@oracle.com>
2019-01-24 16:27:15 +0800

    Update Kubernetes env variables

--- src/test/java/org/jeejeejango/UserServiceApplicationTests.java
+++ src/test/java/org/jeejeejango/UserServiceApplicationTests.java
@@ -1,15 +1,12 @@
 package org.jeejeejango;
 
 import org.jeejeejango.client.TeamClient;
-import org.jeejeejango.entity.Team;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,12 +21,5 @@ public class UserServiceApplicationTests {
     public void contextLoads() {
     }
 
-
-    @Test
-    public void testTeam() {
-        Team team = teamClient.getTeamById(1L);
-        assertNotNull(team);
-    }
-
-
+    
 }
