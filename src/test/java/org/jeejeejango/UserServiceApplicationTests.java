802040e9e081c0ebdf84a46f786862c7e17e9422
jeejeejango <boonping.teo@oracle.com>
2019-01-30 00:59:59 +0800

    Update test

--- src/test/java/org/jeejeejango/UserServiceApplicationTests.java
+++ src/test/java/org/jeejeejango/UserServiceApplicationTests.java
@@ -1,9 +1,8 @@
 package org.jeejeejango;
 
-import org.jeejeejango.client.TeamClient;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,13 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("dev")
 public class UserServiceApplicationTests {
 
-    @Autowired
-    private TeamClient teamClient;
+    @BeforeClass
+    public static void init() {
+        org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.getInstance();
+    }
 
 
     @Test
     public void contextLoads() {
     }
 
-    
+
 }
